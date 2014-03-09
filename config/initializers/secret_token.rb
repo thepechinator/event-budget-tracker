6af6f8d145db270a4564175c6183be91358a12af
ppechin <thepechinator.com>
2014-03-09 16:05:15 -0400

    Initial Rails project commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EventBudgetTracker::Application.config.secret_key_base = '5291aaa9ccc3fd6d02556dcd28c47c41125269020bf2b9f5e787cba62191ae954e95e3d6a44ebd3ab315c722256b37b9efb83044566d72112a8b0cba3d312902'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EventBudgetTracker::Application.config.session_store :cookie_store, key: '_event-budget-tracker_session'
