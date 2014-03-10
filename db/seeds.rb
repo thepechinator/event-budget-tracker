74b985bad3eb104b6b5c70743bccb73ad64cca69
ppechin <thepechinator.com>
2014-03-09 20:37:36 -0400

    Adding seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |i|
+	event = Event.create( name: "Event #{i}", date: "#{Time.now + i.days}", description: "Description #{i}")
+
+	5.times do |j|
+		spending_pool = SpendingPool.new( source: "Source #{j}", amount: 1000 + j, description: "Spending #{j}" )
+		event.spending_pools << spending_pool
+	end
+
+	5.times do |j|
+		expense = Expense.new( total: 500 + j, description: "Expense description #{j}", due_date: "#{Time.now + j.months}")
+
+		5.times do |k|
+			payment = Payment.new( amount: 250 + k, description: "Payment description #{k}", date: "#{Time.now + k.days}" )
+			expense.payments << payment
+		end
+
+		event.expenses << expense
+	end
+
+	event.save!
+end
